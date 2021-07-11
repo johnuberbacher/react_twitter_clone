fc3d9d14a9dccc157635c1035e8a79db78577672
John Uberbacher <johnuberbacher@gmail.com>
2021-07-11 01:02:47 -0600

    firebase settings changed for heroku deployment

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,15 @@
 import firebase from "firebase";
 
-const firebaseConfig = {};
+// Configure Firebase variables within Heroku => Config Vars
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  appID: process.env.REACT_APP_FIREBASE_APP_ID,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
